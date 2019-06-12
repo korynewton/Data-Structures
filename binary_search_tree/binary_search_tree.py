012b138f1b7a991ad70e89cbe5c56a4fbe321361
Kory Newton <korybnewton@gmail.com>
2019-06-12 13:21:01 -0700

    completes contains method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -23,7 +23,21 @@ class BinarySearchTree:
                 self.right.insert(value)
 
     def contains(self, target):
-        pass
+        # check if self.value is target
+        if self.value == target:
+            return True
+        while self.left and self.right:
+            # check if self.value is less than target: go right
+            if self.value < target:
+                # repeat
+                return self.right.contains(target)
+            # check if self.value is greater than target: go left
+            elif self.value > target:
+                # repeat
+                return self.left.contains(target)
+         # else not in BST return false
+        else:
+            return False
 
     def get_max(self):
         pass
@@ -36,3 +50,11 @@ test = BinarySearchTree(5)
 print(test.value)  # 5
 print(test.left)  # None
 print(test.right)  # None
+test.insert(3)
+test.insert(10)
+print(test.left.value)  # 3
+print(test.right.value)  # 10
+# print(test.contains(3))
+# print(test.contains(10))
+print(test.contains(5))
+print(test.contains(3294))
