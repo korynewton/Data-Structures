dc7b5a714c1d258888ac645af1639b4a8c065a7c
Kory Newton <korybnewton@gmail.com>
2019-06-12 15:23:07 -0700

    end day3, max_heap WIP

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,53 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # if len(self.storage) == 0:
+        #     self.storage.append(value)
+        # else:
+        #     pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
-  def delete(self):
-    pass
+    def delete(self):
+        # if only one item:
+        if len(self.storage) == 1:
+            return self.storage.pop()
 
-  def get_max(self):
-    pass
+        current_max = self.storage.pop(0)
 
-  def get_size(self):
-    pass
+        # sift down max node
+        self._sift_down(0)
 
-  def _bubble_up(self, index):
-    pass
+        return current_max
 
-  def _sift_down(self, index):
-    pass
+    def get_max(self):
+        return self.storage[0]
+
+    def get_size(self):
+        return len(self.storage)
+
+    def _bubble_up(self, index):
+        # keep bubbling up until we've either reached the top of the heap or a parent that is grater than node
+        parent = (index - 1)//2
+
+        while index > 0:
+            if self.storage[index] > self.storage[parent]:
+                # swap em
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+
+                # update index for next loop, node we are bubbling is now at index where parent was
+                index = parent
+            else:
+                # break out of while loop because it is in correct spot
+                break
+
+    def _sift_down(self, index):
+        pass
+
+
+test = Heap()
+print(len(test.storage))
+test.insert(5)
+print(len(test.storage))
